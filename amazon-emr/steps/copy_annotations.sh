e795b9f93d8a8076e56faa3d87927348cca0dc4e
Kunjias <41348521+Kunjias@users.noreply.github.com>
2020-01-17 02:17:24 -0800

    Initial markdown commit to copy_annotations.sh

---
+++ amazon-emr/steps/copy_annotations.sh
@@ -0,0 +1,16 @@
+# copy_annotations.sh
+latest's comment on original repo's commit:
+```
+Initial commit of the provisioning scripts for an EMR cluster launch.
+OBJECTIVE:
+The purpose of this script is to copy the required genome annotations into the Master node.
+```
+- line 30-33, 56-58
+
+```echo``` (print date-time in month-day-year hour-min-second format)
+
+Set data and application directories in the worker node, create directories of ```APPS_DIR``` and ```BIODATA_DIR```.
+
+```# Copy annotations (genome sequence reference names) into the cluster```
+
+Print date-time and ‘Done’
